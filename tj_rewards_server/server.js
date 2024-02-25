c7b71386e7d7b584251d4ef032384d9e5d92dea9
James McAleer <jamesjmac11@icloud.com>
2024-02-25 18:36:03 -0500

    troubleshoot app.listen

--- tj_rewards_server/server.js
+++ tj_rewards_server/server.js
@@ -131,7 +131,7 @@ app.use((err, req, res, next) => {
     res.status(500).send('something broke!')
 })
 
-app.listen(8080, '13.58.100.179', () => {
+app.listen(8080, '0.0.0.0', () => {
     console.log('Server is running on port 8080')
 })
 
